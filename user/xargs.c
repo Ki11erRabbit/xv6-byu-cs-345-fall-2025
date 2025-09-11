0f1b1826480c12ab7ac357186116555196244e05
Alec Davis <unlikelytitan@gmail.com>
2025-09-11 11:25:49 -0600

    improved and simplified xargs

--- user/xargs.c
+++ user/xargs.c
@@ -7,35 +7,39 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
-void lines(char *input, int input_length, char ***output, int *output_length) {
-    int offset = 0;
-    int offset_length = 0;
-    char *buffer[512] = {0};
-    int buffer_length = 0;
-    while (offset + offset_length < input_length) {
-        if (input[offset + offset_length] == '\n' || offset + offset_length == input_length) {
-            buffer[buffer_length] = malloc(sizeof(char) * (offset_length + 1));
-            memcpy(buffer[buffer_length], input + offset, offset_length * sizeof(char));
-            buffer[buffer_length][offset_length] = '\0';
-            buffer_length++;
-            offset = offset + offset_length + 1;
-            offset_length = 1;
-        }
-        offset_length++;
-    }
 
-    buffer[buffer_length] = malloc(sizeof(char) * strlen(input + offset));
-    memcpy(buffer[buffer_length], input + offset, strlen(input + offset));
-    buffer[buffer_length][strlen(input + offset)] = '\0';
-    buffer_length++;
+void mark_newlines(char *input, int input_length, char **output) {
+	*output = malloc(input_length + 2); // add null terminator and extra null to know when to stop
+	(*output)[input_length + 2] = '\0';
+	(*output)[input_length + 1] = '\0';
+	memcpy(*output, input, input_length);
+	for (int i = 0; i < input_length; i++) {
+		if ((*output)[i] == '\n') {
+			(*output)[i] = '\0';
+		} else if ((*output)[i] == '\0') {
+			break;
+		}
+	}
+}
 
+void exec_for_each(char **argv, int argc, char *lines) {
+	char **args = malloc((argc + 1) * sizeof(char *));
+	args[argc + 1] = (char *)0;
 
-    *output = malloc(sizeof(char*) * buffer_length);
-    *output_length = buffer_length;
-    for (int i = 0; i < buffer_length; i++) {
-        (*output)[i] = buffer[i];
-    }
-    return;
+	memcpy(args, argv + 1, (argc - 1) * sizeof(char *));
+
+	while (1) {
+		args[argc - 1] = lines;
+		if (fork() == 0) {
+			exec(args[0], args);
+		} else {
+			wait((void*)0);
+		}
+		lines += strlen(lines) + 1;
+		if (lines[0] == '\0') {
+			break;
+		}
+	}
 }
 
 
@@ -55,29 +59,10 @@ int main(int argc, char *argv[]){
         }
         total_read += result;
     }
-    char **args;
-    int args_length = 0;
+	char *lines = (char*)0;
+	mark_newlines(input, total_read, &lines);
 
-    lines(input, strlen(input), &args, &args_length);
-    int all_args_length = argc + 1;
-    char **all_args = malloc(sizeof(char*) * all_args_length);
-    all_args[all_args_length] = (char *)0;
-
-    for (int i = 1; i < argc; i++) {
-        all_args[i - 1] = argv[i];
-    }
-
-    for (int i = 0; i < args_length; i++) {
-        //printf("%s\n", args[i]);
-        all_args[argc - 1] = args[i];
-        if (fork() == 0) {
-            int pid = 0;
-            wait(&pid);
-        } else {
-            exec(argv[1], all_args);
-        }
-    }
-
+	exec_for_each(argv, argc, lines);
 
     exit(0);
 }
