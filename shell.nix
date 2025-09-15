0e14807daf446c0e0b098d3f5521b2f0585db90b
Alec Davis <unlikelytitan@gmail.com>
2025-09-15 14:31:50 -0600

    did the gdb exercise

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    export PATH=${pkgs.pkgsCross.riscv64-embedded.buildPackages.gcc}/bin${pkgs.pkgsCross.riscv64-embedded.buildPackages.binutils}/bin:$PATH
+    export PATH=${pkgs.pkgsCross.riscv64-embedded.buildPackages.gcc}/bin:${pkgs.pkgsCross.riscv64-embedded.buildPackages.binutils}/bin:${pkgs.pkgsCross.riscv64-embedded.buildPackages.gdb}/bin:$PATH
     export TOOLPREFIX=riscv64-none-elf-
   '';
 }
