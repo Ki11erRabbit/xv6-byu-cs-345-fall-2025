ff7cf9b2f34e810b3c4a5ce4decc1e9f6c47eb7a
Alec Davis <unlikelytitan@gmail.com>
2025-09-16 12:00:18 -0600

    found the location of the secret message

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,24 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+
+void print_secret(int fd, char *secret) {
+  write(fd, secret, 40);
+}  
+
 int
 main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *heap = sbrk(PGSIZE * 32);
+  // heap = heap + 9 * PGSIZE;
+  // heap += 32;
+  int i = 0;
+  while (!(heap[i - 1] == 'm' && heap[i] == 'y')) {
+    i++;
+  }
+  print_secret(1, heap + i + 1);
+  print_secret(2, heap + i + 1);
   exit(1);
 }
