d58d31e6cf97131416f939f9b36884eae98ef40a
Alec Davis <unlikelytitan@gmail.com>
2025-09-16 12:35:19 -0600

    found the location of static strings

--- user/attack.c
+++ user/attack.c
@@ -14,13 +14,14 @@ main(int argc, char *argv[])
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
   char *heap = sbrk(PGSIZE * 32);
-  // heap = heap + 9 * PGSIZE;
+  //heap = (char*)16384; // The return value to sbrk from using trace on attacktest
+  heap = heap + 9 * PGSIZE;
   // heap += 32;
   int i = 0;
   while (!(heap[i - 1] == 'm' && heap[i] == 'y')) {
     i++;
   }
-  print_secret(1, heap + i + 1);
-  print_secret(2, heap + i + 1);
+  print_secret(1, heap + i - 2);
+  print_secret(2, heap + i - 2);
   exit(1);
 }
