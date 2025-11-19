9c72c9fe96e8ef1adcb2f7f837ae3403485cb37c
Alec Davis <unlikelytitan@gmail.com>
2025-11-18 17:55:54 -0700

    added vma_list and vma_item

--- kernel/proc.h
+++ kernel/proc.h
@@ -79,6 +79,14 @@ struct trapframe {
   /* 280 */ uint64 t6;
 };
 
+
+struct vma_item {
+  void *address;
+  int length;
+  uint permissions;
+  struct file *file;
+};  
+
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
@@ -104,4 +112,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct vma_item vma_list[MAXVMA];
+  int vma_next;
 };
